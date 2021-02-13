110d3ee94cf06c4890f734e6643755e16022dd7a
selim <selimyesilkaya@gmail.com>
2021-02-13 22:40:54 +0300

    add keyboard event listener and move engine related code to separate class

---
+++ cli-game/Engine/Engine.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Game.Engine
+{
+    public class GameEngine
+    {
+        [DllImport("user32.dll")]
+        static extern short GetKeyState(VirtualKeyStates nVirtKey);
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern SafeFileHandle CreateFile(
+            string fileName,
+            [MarshalAs(UnmanagedType.U4)] uint fileAccess,
+            [MarshalAs(UnmanagedType.U4)] uint fileShare,
+            IntPtr securityAttributes,
+            [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+            [MarshalAs(UnmanagedType.U4)] int flags,
+            IntPtr template);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteConsoleOutput(
+            SafeFileHandle hConsoleOutput,
+            CharInfo[] lpBuffer,
+            Coord dwBufferSize,
+            Coord dwBufferCoord,
+            ref SmallRect lpWriteRegion);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SmallRect
+        {
+            public short Left;
+            public short Top;
+            public short Right;
+            public short Bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Coord
+        {
+            public short X;
+            public short Y;
+
+            public Coord(short X, short Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+        };
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct CharUnion
+        {
+            [FieldOffset(0)] public char UnicodeChar;
+            [FieldOffset(0)] public byte AsciiChar;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct CharInfo
+        {
+            [FieldOffset(0)] public CharUnion Char;
+            [FieldOffset(2)] public short Attributes;
+        }
+
+        private readonly SafeFileHandle m_FileHandle;
+        private readonly CharInfo[] m_DrawBuffer;
+        private readonly SmallRect m_SmallRect;
+
+        private readonly int m_Width;
+        private readonly int m_Height;
+
+        public GameEngine(short width, short height)
+        {
+            CreateBuffer(width, height, out m_FileHandle, out m_DrawBuffer, out m_SmallRect);
+        }
+
+        public short GetKeyStates(VirtualKeyStates keyState)
+        {
+            return GetKeyState(keyState);
+        }
+
+        public void Draw()
+        {
+            DrawBuffer(m_FileHandle, m_DrawBuffer, m_SmallRect);
+        }
+
+        public void Set(int x, int y, char c, CharAttribute attr = CharAttribute.FOREGROUND_RED)
+        {
+            var index = x + y * m_Width;
+            var ci = m_DrawBuffer[index];
+            ci.Attributes = (short)attr;
+            ci.Char.UnicodeChar = c;
+            m_DrawBuffer[index] = ci;
+        }
+
+        public void Set(int x, int y, byte b, CharAttribute attr = CharAttribute.FOREGROUND_RED)
+        {
+            var index = x + y * m_Width;
+            var ci = m_DrawBuffer[index];
+            ci.Attributes = (short)attr;
+            ci.Char.AsciiChar = b;
+            m_DrawBuffer[index] = ci;
+        }
+
+        private static void DrawBuffer(SafeFileHandle handle, CharInfo[] buffer, SmallRect rect)
+        {
+            WriteConsoleOutput(handle, buffer,
+                new Coord() { X = rect.Right, Y = rect.Bottom },
+                new Coord() { X = rect.Left, Y = rect.Top },
+                ref rect);
+        }
+
+        private static void CreateBuffer(short width, short height, out SafeFileHandle handle, out CharInfo[] buffer, out SmallRect rect)
+        {
+            handle = CreateFile("CONOUT$", 0x40000000, 2, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
+            buffer = new CharInfo[width * height];
+            rect = new SmallRect() { Left = 0, Top = 0, Right = width, Bottom = height };
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    buffer[x + width * y] = new CharInfo
+                    {
+                        Attributes = 3,
+                        Char = new CharUnion
+                        {
+                            AsciiChar = 40,
+                        }
+                    };
+                }
+            }
+        }
+    }
+}
